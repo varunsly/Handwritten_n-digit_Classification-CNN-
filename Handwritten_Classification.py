fc1759b3ad5ab112b1bc7b0d81491a72f45c7941
varunsly <vroon.amgotra11@outlook.com>
2020-04-08 16:54:08 +0530

    CNN-Code

---
+++ Handwritten_Classification.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+from keras.layers import Dense,Dropout,Activation,Flatten
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPooling2D
+from  keras.datasets import mnist
+from keras.layers.normalization import BatchNormalization
+from keras.utils import np_utils
+
+
+#Data from MNIST dataset(Subset of NIST)
+(X_train, Y_train),(X_test, Y_test)=mnist.load_data()
+
+#Sample Graph from the dataset
+plt.imshow(X_train[0], cmap='gray' )
+plt.title('Class'+str(Y_train[0]))
+
+feature_train=X_train.reshape(X_train.shape[0],28,28,1)
+feature_test=X_test.reshape(X_test.shape[0],28,28,1)
+
+feature_train=feature_train.astype("float32")
+feature_test=feature_test.astype("float32")
+
+#Data noramalization for the Neural Networks
+feature_test/=255
+feature_train/=255
+
+#Data Noramlization for the output class
+target_train= np_utils.to_categorical(Y_train,10)
+target_test= np_utils.to_categorical(Y_test,10)
+
+#Model is being initiated
+model= Sequential()
+
+#Making Dense Network
+model.add(Conv2D(32, (3,3),input_shape=(28,28,1)))
+model.add(Activation('relu'))
+
+#Function to make the algo run faster by making appropriate changes
+model.add(BatchNormalization())
+
+model.add(Conv2D(32,(3,3)))
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(32,(3,3)))
+model.add(Activation('relu'))
+
+model.add(BatchNormalization())
+
+model.add(Conv2D(64,(3,3)))
+model.add(Activation('relu'))
+
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+#Model Flateening for having input
+model.add(Flatten())
+
+model.add(BatchNormalization())
+model.add(Dense(500))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+#Dropout is being used to reduce overfitting
+model.add(Dropout(0.4))
+
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(feature_train,target_train,batch_size=126, epochs=2, validation_data=(feature_test,target_test))
+
+score=model.evaluate(feature_test,target_test)
+
+print("Test Accuracy: %.2f" %score[1])
+
